405444478dda5298b2c9427539e90514d5b07a4c
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-23 04:22:04 +0900

    [MODIFY] PUTMAPPING TO POSTMAPPING FOR CORRECTING THE ERROR

--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Service;
 public class PostsService {
   private final PostsRepository postsRepository;
 
+  //트랜잭션은 데이터베이스 상태를 변환시키는 하나의 논리적인 기능의 작업 단위이다.
+  //Transaction은 2개 이상의 쿼리를 하나의 커넥션으로 묶어 DB에 전송하고, 이 과정에서 에러가 발생할 경우 자동으로 모든 과정을 원래대로 되돌려 놓는다.
+  //이러한 과정을 구현하기 위해 Transaction은 하나 이상의 쿼리를 처리할 때 동일한 Connection 객체를 공유하도록 한다.
   @Transactional
   public Long save(PostsSaveRequestDto requestDto) {
     return postsRepository.save(requestDto.toEntity()).getId();
--- src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
@@ -1,5 +1,65 @@
 package com.jojoldu.book.springboot.web;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import java.util.List;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) //랜덤포트를 실행한다.
 public class PostsApiControllerTest {
 
+  @LocalServerPort //로컬 내부서버의 포트를 가져온다.
+  private int port;
+
+  @Autowired
+  private TestRestTemplate restTemplate; //REST API 응답을 동기방식으로 처리하는 RestTemplate의 테스트이다. 서블릿 컨테이너가 존재한다.
+
+  @Autowired
+  private PostsRepository postsRepository;
+
+  @After
+  public void tearDown() throws Exception {
+    postsRepository.deleteAll();
+  }
+
+  @Test
+  public void Posts가_등록된다() throws Exception {
+    //given
+    String title = "title";
+    String content = "content";
+    String author = "author";
+    PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                                                    .title(title)
+                                                    .content(content)
+                                                    .author(author)
+                                                    .build();
+    String url = "http://localhost:" + port + "api/v1/posts";
+
+    //when
+    ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+    //ResponseEntity는 HttpEntity를 상속받음으로써 HttpHeader와 body를 가질 수 있다.
+    //restTemplate.postForEntity은 POST 요청을 보내고 결과로 ResponseEntity로 반환받는다.
+
+    //then
+    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(responseEntity.getBody()).isGreaterThan(0L); //Long으로 넘어오는 바디가 0L(0)보다 큰가?
+
+    List<Posts> all = postsRepository.findAll();
+    assertThat(all.get(0).getTitle()).isEqualTo(title);
+    assertThat(all.get(0).getContent()).isEqualTo(content);
+    assertThat(all.get(0).getAuthor()).isEqualTo(author);
+  }
 }
