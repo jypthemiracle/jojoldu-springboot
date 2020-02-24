5b85b90e183328291e82e83ec559b167d507f0fc
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-24 16:46:16 +0900

    [ADD] IndexControllerTest: testifying whether it contains the message that is intended to be contained - utilizing TestRestTemplate.getForObject to get body of the document.

--- src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -1,5 +1,28 @@
 package com.jojoldu.book.springboot.web;
 
-public class IndexControllerTest {
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class IndexControllerTest {
+  @Autowired
+  private TestRestTemplate testRestTemplate;
+
+  @Test
+  public void 메인페이지가_로딩된다() {
+    //when
+    String body = this.testRestTemplate.getForObject("/", String.class);
+    //getForObject: Retrieve a representation by doing a GET on the URI template.
+    //then
+    assertThat(body).contains("Welcome to New World");
+    //TestRestTemplate으로 RestTemplate를 Test해보았을 때, 넘겨주는 body 문자열에 우리가 기대하는 값이 있으면 된다.
+  }
 }
