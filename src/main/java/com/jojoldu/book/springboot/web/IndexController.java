6e9bb37e8706a04a88e21a76b1a851120cb36848
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-25 13:21:18 +0900

    [ADD] IndexController : getmapping for save/update/rendering index - postsUpdate to map with "/posts/update/{Id}", find article by Id via Dto, update Model. - postsSave to map with "/posts/save", return posts-save mustache. - index to map with default page, call findAllDesc and throw it to model for print.

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,14 +1,33 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.service.posts.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor //private field에서만 생성자를 필수로 만든다.
 @Controller
 public class IndexController {
   //머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할 때 앞의 경와 뒤의 파일 확장자는 자동으로 지정된다.
+  private final PostsService postsService;
+
   @GetMapping("/") //앞의 경로
-  public String index() {
+  public String index(Model model) {
+    model.addAttribute("posts", postsService.findAllDesc());
     return "index"; //파일 확장자. 이는 View Resolver가 처리한다.
     //View Resolver는 URL 요청의 결과를 전달할 타입과 값을 지정하는 관리자 격으로 생각해볼 수 있다.
+  }
+  @GetMapping("/posts/save")
+  public String postsSave() {
+    return "posts-save";
+  }
+  @GetMapping("/posts/update/{Id}")
+  public String postsUpdate(@PathVariable Long Id, Model model) {
+    PostsResponseDto responseDto = postsService.findById(Id);
+    model.addAttribute("post", responseDto);
+    return "posts-update";
   }
 }
