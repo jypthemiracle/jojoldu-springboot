2b4da9b63068a727ae52f839197f243952d11b93
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-03-29 21:40:02 +0900

    test: @WebMvcTest에서 CustomOAuth2UserService을 찾을 수 없는 문제 해결 - HelloControllerTest에서는 @WebMvcTest를 이용하는데도 비슷한 에러가 발생했다. - 이는 @WebMvcTest가 CustomOAuth2UserService를 스캔하지 않기 때문이라고 한다. - @WebMvcTest는 WebSecurityConfigurerAdapter, WebMvcConfigurer를 비롯한 @ControllerAdvice, @Controller를 읽는다. - 그러니 SecurityConfig는 읽었지만, SecurityConfig를 생성하기 위해 필요한 CustomOAuth2UserService는 읽을 수가 없어 앞에서와 같이 에러가 발생한 것이다. - 그래서 이 문제를 해결하기 위해 스캔 대상에서 SecurityConfig를 제거하였다. - 그리고 @WithMockUser를 사용해서 가짜 사용자를 생성한다. - 에러가 발생하는데 이는 @EnableJpaAuditing로 인해 발생한다. 최소 하나의 @Entity가 필요한데, @WebMvcTest이다 보니 당연히 존재하지 않기 때문이다. - 따라서 @EnableJpaAuditing가 @SpringBootApplication와 함께 있다보니 @WebMvcTest에서도 스캔하게 된 것이다. - 그래서 @EnableJpaAuditing과 @SpringBootApplication 둘을 분리하겠다. - Application.java에서 @EnableJpaAuditing을 제거하고, config 패키지에 JpaCOnfig를 생성하여 @EnableJpaAuditing을 추가해본다.

--- src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
@@ -1,10 +1,15 @@
 package com.jojoldu.book.springboot;
 
+import com.jojoldu.book.springboot.config.auth.SecurityConfig;
 import com.jojoldu.book.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,13 +23,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class) //테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킨다.
 //여기서는 SpringRunner라는 스프링 실행자를 실행하는데, 이는 스프링 부트 테스트와 JUnit 사이의 연결자 역할을 한다.
 
-@WebMvcTest(controllers = HelloController.class) //WebMvc를 테스트하겠다고 선언하는 어노테이션이다.
+@WebMvcTest(controllers = HelloController.class, //@WebMvcTest는 SecurityConfig은 읽지만 CustomOAuth2UserService는 읽지 못한다.
+        excludeFilters = { //따라서 다음과 같이 스캔 대상에서 SecurityConfig을 제거한다. 사실 언제 삭제될지도 모르니 사용하지 말자.
+          @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }) //WebMvc를 테스트하겠다고 선언하는 어노테이션이다.
 
 public class HelloControllerTest {
   @Autowired //Bean을 주입하는 역할을 수행한다.
   private MockMvc mvc; //GET, POST 등 웹 API를 테스트할 때 사용된다. 스프링 MVC 테스트의 시작점이다.
 
   @Test
+  @WithMockUser(roles="USER")
   public void hello가_리턴된다() throws Exception {
     String hello = "hello";
     mvc.perform(get("/hello")) //MockMvc를 통해 /hello 주소로 HTTP GET 요청을 한다. 체이닝이 지원된다.
@@ -33,6 +42,7 @@ public class HelloControllerTest {
   }
 
   @Test
+  @WithMockUser(roles="USER")
   public void helloDto가_리턴된다() throws Exception {
     String name = "hello";
     int amount = 1000;
