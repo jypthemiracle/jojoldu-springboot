18dbf3efa14c1e228ffbed026871b4459cf020ae
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-03-29 10:02:10 +0900

    feat: 세션에 로그인 된 사용자의 정보를 받아오는 별도의 DTO 설계 - DTO를 설계하는 이유는 User 클래스를 세션에 저장하려고 하니 직렬화를 구현하지 않았다는 에러가 나타난다. - 그럼 오류를 해결하기 위해 User 클래스에 직렬화 코드를 넣으면 문제가 해결 될까? - User 클래스는 엔티티이다. 엔티티 클래스는 언제 다른 엔티티와 관계가 형성될 지 모릅니다. - 예를 들어 @OneToMany, @ManyToMany 등 자식 엔티티를 갖고 있다면 직렬화 대상에 자식들까지 포함된다. - 따라서 직렬화 기능을 가진 세션 DTO를 하나 추가로 만드는 것이 낫다고 한다.

---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,28 @@
+package com.jojoldu.book.springboot.config.auth.dto;
+
+import com.jojoldu.book.springboot.domain.user.User;
+import java.io.Serializable;
+
+public class SessionUser implements Serializable {
+  private String name;
+  private String email;
+  private String picture;
+
+  public SessionUser(User user) {
+    this.name = user.getName();
+    this.email = user.getEmail();
+    this.picture = user.getPicture();
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public String getPicture() {
+    return this.picture;
+  }
+}
