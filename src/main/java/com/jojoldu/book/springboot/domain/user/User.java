75ae68b4929cdc3458adcd5708291593fef39a72
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-03-29 16:30:23 +0900

    chore: User 클래스에 @Entity 어노테이션 및 기본 생성자 구현

--- src/main/java/com/jojoldu/book/springboot/domain/user/User.java
+++ src/main/java/com/jojoldu/book/springboot/domain/user/User.java
@@ -2,13 +2,17 @@ package com.jojoldu.book.springboot.domain.user;
 
 import com.jojoldu.book.springboot.domain.BaseTimeEntity;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class User extends BaseTimeEntity {
+
+  public User() {}
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
