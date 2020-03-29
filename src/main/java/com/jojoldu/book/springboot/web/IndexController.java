ab307cbc7d48b5bee4272768d90485da77c0fd86
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-03-29 15:38:11 +0900

    feat: index.mustache에 SessionUser를 전달하기 위해 HttpSession에서 사용자 정보를 받아오고 이를 null 체크 후 전달

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,7 +1,10 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
+import com.jojoldu.book.springboot.domain.user.User;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,10 +16,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
   //머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할 때 앞의 경와 뒤의 파일 확장자는 자동으로 지정된다.
   private final PostsService postsService;
+  private final HttpSession httpSession;
 
   @GetMapping("/") //앞의 경로
   public String index(Model model) {
     model.addAttribute("posts", postsService.findAllDesc());
+    SessionUser user = (SessionUser) httpSession.getAttribute("user");
+    if (user != null) {
+      model.addAttribute("userName", user.getName());
+    }
     return "index"; //파일 확장자. 이는 View Resolver가 처리한다.
     //View Resolver는 URL 요청의 결과를 전달할 타입과 값을 지정하는 관리자 격으로 생각해볼 수 있다.
   }
